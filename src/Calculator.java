021d6ae91ce3fdc80f7c1f0e1c0cfae02ff2fa7a
dangkimlong303 <dangkimlong303@gmail.com>
2018-11-12 11:22:34 +0700

    refactor

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISTION = '/';
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISTION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,8 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        System.out.println(Calculator.calculate(3,3,Calculator.ADDITION));
+        System.out.println(Calculator.calculate(3,3,Calculator.SUBTRACTION));
+        System.out.println(Calculator.calculate(3,3,Calculator.MULTIPLICATION));
+        System.out.println(Calculator.calculate(3,3,Calculator.DIVISTION));
+    }
+}
